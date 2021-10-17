3aac46c88437f6f09b080cf1abd651f4c5754cdd
ezzeldem <ezzelden06@gmail.com>
2021-10-17 06:50:41 +0200

    media -v

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,6 +10,24 @@ $(document).ready(function () {
     autoplaySpeed: 1700,
   });
 
+
+  $(".opne_icon i").click(function () {
+    // opne mobile menu
+    $(".header-section .navBox ").addClass("opne_menuBox");
+    $(".header-section .navBox ul li").addClass("up-a");
+    $(".close-menu").addClass("close-icon");
+    $(".overlay").show();
+  });
+  $(".close-menu i, .overlay").click(function () {
+    $(".header-section .navBox ").removeClass("opne_menuBox");
+   $(".header-section .navBox ul li").removeClass("up-a");
+  $(".close-menu").removeClass("close-icon");
+  $(".overlay").hide();
+ })
+
+
+
+
   // end aos
   AOS.init();
 })
